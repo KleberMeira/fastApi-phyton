9e196740bd188243d8cffed6e19d5d94e6f698b5
Kleber Meira <kleber.lima@compasso.com.br>
2024-04-08 15:46:54 -0300

    feat: add endpoint de login

--- pythonProject/api/v1/endpoints/usuario.py
+++ pythonProject/api/v1/endpoints/usuario.py
@@ -103,6 +103,20 @@ async def get_delete(usuario_id: int, db: AsyncSession = Depends(get_session)):
             raise HTTPException(detail='Usuário não encontrado',
                                 status_code=status.HTTP_404_NOT_FOUND)
 
+#Post Login
+@router.post('/login')
+async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: AsyncSession = Depends(get_session)):
+    usuario = await autenticar(email = form_data.username, senha= form_data.password, db=db)
+
+    if not usuario:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail='Dados de acesso incorretos')
+
+    return JSONResponse(content={"access-token": criar_token_acesso(sub=usuario.id),
+                                 "token_type": "bearer"},
+                        status_code=status.HTTP_200_OK)
+
+
 
 
 
