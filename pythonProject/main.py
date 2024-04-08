f2e0529e21a5ab1df3f7790ffdc1503e2c83e687
Kleber Meira <kleber.lima@compasso.com.br>
2024-04-08 15:52:01 -0300

    feat: ajuste de arquivo main

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -3,7 +3,7 @@ from core.configs import settings
 from api.v1.api import api_router
 
 
-app = FastAPI(title='Cursos FastApi - FastApi SQL Alchemy')
+app = FastAPI(title='Curso FastApi - FastApi Security')
 app.include_router(api_router, prefix=settings.API_V1_STR)
 
 # api/v1/cursos
