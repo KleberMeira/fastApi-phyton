29abbb7d85684d6b3a639c4c0c3a38251a92ac86
Kleber Meira <kleber.lima@compasso.com.br>
2024-04-08 15:38:15 -0300

    feat: endpoint usuario com autenticacao

---
+++ pythonProject/api/v1/endpoints/usuario.py
@@ -0,0 +1,108 @@
+from typing import List, Optional, Any
+from fastapi import APIRouter, status, Depends, HTTPException, Response
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.responses import JSONResponse
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+
+from models.usuario_model import UsuarioModel
+from schemas.usuario_schema import UsuarioSchemaUp, UsuarioSchemaBase, UsuarioSchemaCreate, UsuarioSchemaArtigos
+from core.deps import get_session, get_current_user
+from core.security import gerar_hash_senha
+from core.auth import autenticar, criar_token_acesso
+
+router = APIRouter
+
+# Get Logado
+@router.get('/logado', response_model=UsuarioSchemaBase)
+def get_logado(usuario_logado: UsuarioModel = Depends(get_current_user)):
+    return usuario_logado
+
+#Post Signup
+@router.post('/signup', status_code=status.HTTP_201_CREATED, response_model=UsuarioSchemaBase)
+async def post_usuario(usuario: UsuarioSchemaCreate, db: AsyncSession = Depends(get_session)):
+    novo_usuario: UsuarioModel = UsuarioModel(
+        nome = usuario.nome,
+        sobrenome = usuario.sobrenome,
+        email = usuario.email,
+        senha = gerar_hash_senha(usuario.senha),
+        eh_admin = usuario.eh_admin
+    )
+    async with db as session:
+        session.add(novo_usuario)
+        await session.commit()
+
+        return novo_usuario
+
+#Get Usuarios
+@router.get('/', response_model=UsuarioSchemaBase)
+async def get_usuarios(db:AsyncSession = Depends(get_session)):
+    async with db as session:
+        query: select(UsuarioModel)
+        result: await session.execute(query)
+        usuarios: List[UsuarioSchemaBase] = result.scalars().unique().all()
+
+        return usuarios
+
+#Get Usuario
+@router.get('/{usuario_id}', response_model=UsuarioSchemaArtigos, status_code=status.HTTP_200_OK )
+async def get_usuario(usuario_id: int, db: AsyncSession = Depends(get_session)):
+    async with db as session:
+        query: select(UsuarioModel).filter(UsuarioModel.id == usuario_id)
+        result: await session.execute(query)
+        usuario: UsuarioSchemaArtigos = result.scalars().unique().one_or_none()
+
+        if usuario:
+            return usuario
+        else:
+            raise HTTPException(detail='Usuário não encontrado',
+                                status_code=status.HTTP_404_NOT_FOUND)
+
+#Put Usuario
+@router.put('/{usuario_id}', response_model=UsuarioSchemaBase, status_code=status.HTTP_202_ACCEPTED )
+async def get_usuario(usuario_id: int, usuario: UsuarioSchemaUp, db: AsyncSession = Depends(get_session)):
+    async with db as session:
+        query: select(UsuarioModel).filter(UsuarioModel.id == usuario_id)
+        result: await session.execute(query)
+        usuario_up: UsuarioSchemaBase = result.scalars().unique().one_or_none()
+
+        if usuario_up:
+
+            if usuario.nome:
+                usuario_up.nome = usuario.nome
+            if usuario.sobrenome:
+                usuario_up.sobrenome = usuario.sobrenome
+            if usuario.email:
+                usuario_up.email = usuario.email
+            if usuario.eh_admin:
+                usuario_up.eh_admin = usuario.eh_admin
+            if usuario.senha:
+                usuario_up.senha = gerar_hash_senha(usuario.senha)
+
+            await session.commit()
+
+            return usuario_up
+        else:
+            raise HTTPException(detail='Usuário não encontrado',
+                                status_code=status.HTTP_404_NOT_FOUND)
+
+#Delete Usuario
+@router.delete('/{usuario_id}', status_code=status.HTTP_204_NO_CONTENT )
+async def get_delete(usuario_id: int, db: AsyncSession = Depends(get_session)):
+    async with db as session:
+        query: select(UsuarioModel).filter(UsuarioModel.id == usuario_id)
+        result: await session.execute(query)
+        usuario_del: UsuarioSchemaArtigos = result.scalars().unique().one_or_none()
+
+        if usuario_del:
+            await session.delete(usuario_del)
+            await session.commit()
+            return Response(status_code=status.HTTP_204_NO_CONTENT)
+        else:
+            raise HTTPException(detail='Usuário não encontrado',
+                                status_code=status.HTTP_404_NOT_FOUND)
+
+
+
+
