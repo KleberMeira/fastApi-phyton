1d8b575624a915a5a8fe8af38afd68346620c6fa
Kleber Meira <kleber.lima@compasso.com.br>
2024-04-08 15:49:24 -0300

    feat: ajustando arquivo api com rotas

--- pythonProject/api/v1/api.py
+++ pythonProject/api/v1/api.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
-from api.v1.endpoints import curso
+from api.v1.endpoints import artigo
+from api.v1.endpoints import usuario
 
 api_router = APIRouter()
-api_router.include_router(curso.router, prefix='/cursos', tags=["cursos"])
+api_router.include_router(artigo.router, prefix='/artigos', tags=["artigos"])
+api_router.include_router(usuario.router, prefix='/usuarios', tags=["usuarios"])
 
 # /api/v1/cursos
